00abe8549074fe8dcf812234103263af9c3236b4
Anton Yashin <antoshkoo@gmail.com>
2021-11-04 16:58:47 +0300

    feat: 04 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const { resolve } = require("path");
+const { readdir, copyFile, mkdir } = require("fs/promises");
 
+absPath = resolve(__dirname, "files");
+absCopyPath = resolve(__dirname, "files-copy");
+
+async function copyPath() {
+  await createCopyPath();
+  const files = await readdir(absPath);
+  await copyFiles(files);
+}
+
+async function createCopyPath() {
+  try {
+    await mkdir(absCopyPath, { recursive: true });
+  } catch {
+    console.log("Directory not created");
+  }
+}
+
+async function copyFiles(files) {
+  try {
+    for (file of files) {
+      absFile = resolve(__dirname, "files", file);
+      absNewFile = resolve(__dirname, absCopyPath, file);
+      await copyFile(absFile, absNewFile);
+      console.log(`${file} copied`);
+    }
+  } catch {
+    console.log("Files doesn't copied");
+  }
+}
+
+copyPath();
