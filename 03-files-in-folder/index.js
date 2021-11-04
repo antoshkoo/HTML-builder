b55c659f6db1691613bb4f7458582b9452e05986
Anton Yashin <antoshkoo@gmail.com>
2021-11-04 14:54:51 +0300

    feat: done 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const { resolve, join } = require("path");
+const { stdout } = process;
+
+const absPath = resolve(__dirname, "secret-folder");
+const absFile = (file) => join(__dirname, "secret-folder", file);
+
+const readFiles = fs.readdir(absPath, { withFileTypes: true }, (err, data) => {
+  if (err) throw err;
+  data.forEach((el) => {
+    if (el.isFile()) {
+      getFileInfo(el);
+    }
+  });
+});
+
+const getFileInfo = (file) => {
+  fs.stat(absFile(file.name), (err, stats) => {
+    if (err) throw err;
+    const name = file.name.split(".");
+    stdout.write(`${name[0]} - ${name[1]} - ${stats.size / 1000}kb\n`);
+  });
+};
